f576f255a2f4b2f69bab38b138ff2a95d826a33f
annychang1111 <30671162+annychang1111@users.noreply.github.com>
2018-04-21 02:46:13 -0400

    Add files via upload

---
+++ GDP Sea/sketch.js
@@ -0,0 +1,31 @@
+var allData;
+var allDataC;
+var usGDP;
+var caGDP;
+
+function preload() {
+  allData = loadJSON("http://api.worldbank.org/v2/countries/us/indicators/NY.GDP.MKTP.CD?format=json");
+  allDataC = loadJSON("http://api.worldbank.org/v2/countries/ca/indicators/NY.GDP.MKTP.CD?format=json");
+}
+
+function setup() {
+  createCanvas(1350, 700);
+  frameRate(10);
+  usGDP = allData[1];
+  caGDP = allDataC[1];
+  // console.log(usGDP[1].value);
+  // console.log(loadJSON("http://api.worldbank.org/v2/countries/ca/indicators/NY.GDP.MKTP.CD?format=json"));
+}
+
+function draw() {
+
+  for(var i = 0; i < 50; i++){
+  	var m = map(usGDP[i].value, 942500000000, 18624475000000, 0, 700);
+  	var l = map(usGDP[i].value, 942500000000, 18624475000000, 85, 5);
+  	colorMode(HSL, 100, 100, 100, 1);
+  	fill(53, 100, l, 1);
+  	noStroke();
+  	rect(0, 0, 1350, m + sin(millis()* 0.01 + i));
+  }
+
+}
